d55a98e8f54c969eeed95e542e248521347ae179
Dhiru Sarania <dhirusarania@users.noreply.github.com>
2020-03-21 15:56:44 +0530

    kj

--- customCMS/views.py
+++ customCMS/views.py
@@ -1,4 +1,4 @@
-   from django.shortcuts import render
+from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import api_view, permission_classes
